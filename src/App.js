563e70a821e65a2377729b5838f306fca4e87357
Sergio Rosa <scarlosjazz@gmail.com>
2019-07-16 09:32:04 -0400

    Card Component

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CardList name="Sergio">
+        <CardList monsters={this.state.monsters}>
           {this.state.monsters.map(monster => (
             <h1 key={monster.id}> {monster.name}</h1>
           ))}
