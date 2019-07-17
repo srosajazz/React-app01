bc0bbb879dc21d8a1d8ad49d4286caa4519b8527
Sergio Rosa <scarlosjazz@gmail.com>
2019-07-17 10:02:09 -0400

    Updates

---
+++ precache-manifest.f83e77c8f39878e9c5f33e6295d01a77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c674fca5641677e9bfe7a105877cc14",
+    "url": "/React-app01/index.html"
+  },
+  {
+    "revision": "ec7b676c857f300da831",
+    "url": "/React-app01/static/css/main.7e4fe80a.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/React-app01/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ec7b676c857f300da831",
+    "url": "/React-app01/static/js/main.a8c6e72c.chunk.js"
+  },
+  {
+    "revision": "e17e8771d93c8fe882e7",
+    "url": "/React-app01/static/js/runtime~main.cf9cff70.js"
+  }
+]);
